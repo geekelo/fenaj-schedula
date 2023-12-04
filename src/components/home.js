8b364273f92148d2c01a8f37481082632ae40828
geekelo <efueliht12@gmail.com>
2023-12-04 12:42:26 +0100

    Add signup component th form

---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Signup from '../auth/signup';
+
+function Home() {
+  return (
+    <div>
+      <p>Hello</p>
+      < Signup />
+    </div>
+  );
+}
+
+export default Home;
