83fb7a8d84872c399bac781e705ebe332b5e2ce4
geekelo <efueliht12@gmail.com>
2023-12-06 16:59:22 +0100

    Implement redirection to signup page after signing out

--- src/App.js
+++ src/App.js
@@ -6,16 +6,14 @@ import {
   Routes,
   Route,
   Navigate,
-  Link,
-  useNavigate,
 } from 'react-router-dom';
 import Home from './components/home';
 import Signup from './auth/signup';
 import Login from './auth/login';
 import { checkLoginStatus } from './redux/loginSlice';
+import Navbar from './components/navbar';
 
 function App() {
-  const navigate = useNavigate();
   const dispatch = useDispatch();
   const loginStatus = useSelector((state) => state.login_auths.loggedin) || 'empty';
 
@@ -37,33 +35,11 @@ function App() {
     console.log('Login status changed:', loginStatus);
   }, [loginStatus]);
 
-  const signout = () => {
-    localStorage.clear();
-    navigate('/signup')
-    // After clearing local storage, you may want to force a checkLoginStatus
-    // dispatch(checkLoginStatus());
-  };
-
   if (loginStatus !== 'empty') {
     return (
       <div>
         <Router>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/home">Home</Link>
-              </li>
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
-              <li>
-                <Link to="/signup">Sign up</Link>
-              </li>
-              <li>
-                <button onClick={signout}>Sign Out</button>
-              </li>
-            </ul>
-          </nav>
+          <Navbar />
           <Routes>
             <Route
               path="/"
