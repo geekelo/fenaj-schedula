63af731a6ac0caa81531e3dfea9a0ff229d2a765
geekelo <efueliht12@gmail.com>
2023-12-12 09:44:38 +0100

    Add navbar  styles

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import axios from 'axios';
-import { useNavigate, Link } from 'react-router-dom';
+import {
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+  FaEnvelope,
+} from 'react-icons/fa';
+import { useNavigate, NavLink } from 'react-router-dom';
+import '../stylesheets/navbar.css';
+import logo from '../assets/logo.png';
 
 function Navbar() {
   const navigate = useNavigate();
@@ -26,30 +34,38 @@ function Navbar() {
 
   return (
     <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/home">Home</Link>
+      <nav className="navbar">
+        <ul className="menuContainer">
+          <img src={logo} alt="logo" className="logoImage" />
+          <li className="menuItems">
+            <NavLink to="/home" className="menuLinks" activeClassName="active">Home</NavLink>
           </li>
-          <li>
-            <Link to="/login">Login</Link>
+          <li className="menuItems">
+            <NavLink to="/add-spa-session" className="menuLinks" activeClassName="active">Add Session</NavLink>
           </li>
-          <li>
-            <Link to="/signup">Sign up</Link>
+          <li className="menuItems">
+            <NavLink to="/my-reservations" className="menuLinks" activeClassName="active">Reservations</NavLink>
           </li>
-          <li>
-            <Link to="/add-spa-session">Add Spa Session</Link>
+          <li className="menuItems">
+            <NavLink to="/delete-spa-sessions" className="menuLinks" activeClassName="active">Delete Sessions</NavLink>
           </li>
-          <li>
-            <Link to="/my-reservations">My Reservations</Link>
+          <li className="menuItems">
+            <NavLink to="/login" className="menuLinks" activeClassName="active">Login</NavLink>
           </li>
-          <li>
-            <Link to="/delete-spa-sessions">Delete Spa Sessions</Link>
-          </li>
-          <li>
-            <button type="submit" onClick={signout}>Sign Out</button>
+          <li className="menuItems">
+            <button type="submit" className="menuLinks" onClick={signout}>Sign Out</button>
           </li>
         </ul>
+
+        <div>
+          <div className="iconsSection">
+            <p><FaFacebook /></p>
+            <p><FaTwitter /></p>
+            <p><FaInstagram /></p>
+            <p><FaEnvelope /></p>
+          </div>
+          <p>© Copyrights 2023</p>
+        </div>
       </nav>
     </div>
   );
