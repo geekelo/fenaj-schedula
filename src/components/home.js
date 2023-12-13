029b36b36101d8ca32fb7b53671b483553c53057
Adeyemi Dada <yemidada@gmail.com>
2023-12-13 10:02:50 +0100

    Update home file

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,13 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
 import { displayItems } from '../redux/displayItemSlice';
 import EachItem from './eachitem';
+
+import '../stylesheets/eachItem.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 function Home() {
   const dispatch = useDispatch();
@@ -11,10 +17,25 @@ function Home() {
     // Call checkLoginStatus when the component mounts
     dispatch(displayItems());
   }, [dispatch]);
-  console.log(items);
+
   return (
-    <div>
-      {items.map((each) => <EachItem key={each.id} eachitem={each} />)}
+    <div className="eachItem">
+      <div className="item_slider">
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          navigation
+          loop
+          modules={[Navigation]}
+          className="mySwiper"
+        >
+          {items.map((each) => (
+            <SwiperSlide key={each.id}>
+              <EachItem key={each.id} eachitem={each} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 }
